831c7901192f803a9fda7ade0241dbf19bdef6d8
Aris Chow <aris.chow@seedlinktech.com>
2021-07-14 14:58:19 +0800

    refactor: debug msg

--- cr.sh
+++ cr.sh
@@ -258,6 +258,7 @@ release_charts() {
 update_index() {
     local args=(-o "$owner" -r "$repo" -c "$charts_repo_url" --push)
     args+=(--packages-with-index)
+    echo "update_index args: '$args'"
     if [[ -n "$config" ]]; then
         args+=(--config "$config")
     fi
